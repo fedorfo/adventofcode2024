7785c621873051bef21cf49be21d63f710f8ea0a
fedor.fominyh <fedor@anna.money>
2024-12-01 05:11:08 +0000

    Day01

--- Puzzles/Day01.cs
+++ Puzzles/Day01.cs
@@ -4,5 +4,14 @@ using Base;
 
 public class Day01 : PuzzleBase
 {
-    public override void Solve() => Console.WriteLine(ReadLines().Single().Split(' ').Select(int.Parse).Sum());
+    public override void Solve()
+    {
+        var pairs = ReadLines().Select(x=>x.Split("   ").ToArray()).ToArray();
+        var left = pairs.Select(x => int.Parse(x[0])).Order().ToArray();
+        var right = pairs.Select(x => int.Parse(x[1])).Order().ToArray();
+        Console.WriteLine(left.Zip(right, (a, b) => Math.Abs(a - b)).Sum());
+
+        var dict = right.GroupBy(x => x).ToDictionary(x => x.Key, x => x.Count());
+        Console.WriteLine(left.Select(x=>(long)dict.GetValueOrDefault(x, 0)*x).Sum());
+    }
 }
