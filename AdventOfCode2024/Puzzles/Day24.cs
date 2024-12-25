ab2b5bf0458bcff7d4f2d8d7ecfb10fc1a756b46
fedor.fominyh <fedor@anna.money>
2024-12-25 03:52:21 +0000

    speed up Day24

--- AdventOfCode2024/Puzzles/Day24.cs
+++ AdventOfCode2024/Puzzles/Day24.cs
@@ -6,11 +6,11 @@ using MoreLinq;
 
 public class Day24 : PuzzleBase
 {
-    private static bool AreGatesCorrect(List<Gate> gates)
+    private static bool AreGatesCorrect(Device device)
     {
         for (var i = 0; i <= 44; i++)
         {
-            if (CalculateZ((long)1 << i, (long)1 << i, gates) != (long)1 << (i + 1))
+            if (CalculateZ((long)1 << i, (long)1 << i, device) != (long)1 << (i + 1))
             {
                 return false;
             }
@@ -20,7 +20,7 @@ public class Day24 : PuzzleBase
         {
             var x = Random.Shared.NextInt64((long)1 << 45);
             var y = Random.Shared.NextInt64((long)1 << 45);
-            if (CalculateZ(x, y, gates) != x + y)
+            if (CalculateZ(x, y, device) != x + y)
             {
                 return false;
             }
@@ -31,15 +31,9 @@ public class Day24 : PuzzleBase
 
     private static bool TryFillWires(
         Dictionary<string, bool> values,
-        List<Gate> gates
+        Device device
     )
     {
-        var gateByOut = gates.ToDictionary(x => x.Out);
-        var edges = gates
-            .Select(x => (x.In1, x.Out))
-            .Concat(gates.Select(x => (x.In2, x.Out)))
-            .GroupBy(x => x.Item1)
-            .ToDictionary(x => x.Key, x => x.Select(y => y.Item2).ToArray());
         var queue = new Queue<string>();
         values.Keys.ForEach(x => queue.Enqueue(x));
         var visited = new Dictionary<string, int>();
@@ -48,16 +42,17 @@ public class Day24 : PuzzleBase
             var v = queue.Dequeue();
             if (!values.ContainsKey(v))
             {
-                var gate = gateByOut[v];
+                var gate = device.GetGateByOut(v);
                 values[gate.Out] = gate.Operator switch
                 {
                     "AND" => values[gate.In1] & values[gate.In2],
                     "OR" => values[gate.In1] | values[gate.In2],
-                    "XOR" => values[gate.In1] ^ values[gate.In2]
+                    "XOR" => values[gate.In1] ^ values[gate.In2],
+                    _ => throw new ArgumentOutOfRangeException()
                 };
             }
 
-            foreach (var u in edges.GetValueOrDefault(v, []))
+            foreach (var u in device.GetOutsByIn(v))
             {
                 visited.TryAdd(u, 0);
                 visited[u]++;
@@ -79,7 +74,7 @@ public class Day24 : PuzzleBase
         return true;
     }
 
-    private static long? CalculateZ(long x, long y, List<Gate> gates)
+    private static long? CalculateZ(long x, long y, Device device)
     {
         var values = new Dictionary<string, bool>();
         for (var i = 0; i <= 44; i++)
@@ -88,7 +83,7 @@ public class Day24 : PuzzleBase
             values[$"y{i:00}"] = (y & ((long)1 << i)) != 0;
         }
 
-        if (!TryFillWires(values, gates))
+        if (!TryFillWires(values, device))
         {
             return null;
         }
@@ -110,33 +105,33 @@ public class Day24 : PuzzleBase
         var gates = ReadBlockLines()
             .Select(x => x.ExtractTokens(' ', '-', '>').ToArray())
             .Select(x => new Gate(x[0], x[2], x[1], x[3]))
-            .ToList();
-        Console.WriteLine(CalculateZ(GetNumber("x", wireValues), GetNumber("y", wireValues), gates));
+            .ToArray();
+        var device = new Device(gates);
+        Console.WriteLine(CalculateZ(GetNumber("x", wireValues), GetNumber("y", wireValues), device));
         var candidates = new List<(Gate, Gate)>();
         var sw = Stopwatch.StartNew();
         for (var i = 0; i <= 44; i++)
         {
-            if (CalculateZ((long)1 << i, (long)1 << i, gates) == (long)1 << (i + 1))
+            if (CalculateZ((long)1 << i, (long)1 << i, device) == (long)1 << (i + 1))
             {
                 continue;
             }
 
-            for (var j = 0; j < gates.Count; j++)
+            foreach (var gate1 in device.Gates)
             {
-                for (var k = j + 1; k < gates.Count; k++)
+                foreach (var gate2 in device.Gates.Where(x => x.Id > gate1.Id))
                 {
-                    var gate1 = gates[j];
-                    var gate2 = gates[k];
-                    (gate1.Out, gate2.Out) = (gate2.Out, gate1.Out);
-                    if (CalculateZ((long)1 << i, (long)1 << i, gates) == (long)1 << (i + 1))
+                    device.SwapOut(gate1, gate2);
+                    if (CalculateZ((long)1 << i, (long)1 << i, device) == (long)1 << (i + 1))
                     {
                         candidates.Add((gate1, gate2));
                     }
 
-                    (gate1.Out, gate2.Out) = (gate2.Out, gate1.Out);
+                    device.SwapOut(gate1, gate2);
                 }
             }
         }
+
         Console.WriteLine(sw.Elapsed);
 
         var sw2 = Stopwatch.StartNew();
@@ -162,12 +157,12 @@ public class Day24 : PuzzleBase
                 continue;
             }
 
-            (candidates[i].Item1.Out, candidates[i].Item2.Out) = (candidates[i].Item2.Out, candidates[i].Item1.Out);
-            (candidates[j].Item1.Out, candidates[j].Item2.Out) = (candidates[j].Item2.Out, candidates[j].Item1.Out);
-            (candidates[k].Item1.Out, candidates[k].Item2.Out) = (candidates[k].Item2.Out, candidates[k].Item1.Out);
-            (candidates[l].Item1.Out, candidates[l].Item2.Out) = (candidates[l].Item2.Out, candidates[l].Item1.Out);
+            device.SwapOut(candidates[i].Item1, candidates[i].Item2);
+            device.SwapOut(candidates[j].Item1, candidates[j].Item2);
+            device.SwapOut(candidates[k].Item1, candidates[k].Item2);
+            device.SwapOut(candidates[l].Item1, candidates[l].Item2);
 
-            if (AreGatesCorrect(gates))
+            if (AreGatesCorrect(device))
             {
                 Console.WriteLine(
                     string.Join(",",
@@ -181,18 +176,62 @@ public class Day24 : PuzzleBase
                 return;
             }
 
-            (candidates[i].Item1.Out, candidates[i].Item2.Out) = (candidates[i].Item2.Out, candidates[i].Item1.Out);
-            (candidates[j].Item1.Out, candidates[j].Item2.Out) = (candidates[j].Item2.Out, candidates[j].Item1.Out);
-            (candidates[k].Item1.Out, candidates[k].Item2.Out) = (candidates[k].Item2.Out, candidates[k].Item1.Out);
-            (candidates[l].Item1.Out, candidates[l].Item2.Out) = (candidates[l].Item2.Out, candidates[l].Item1.Out);
+            device.SwapOut(candidates[i].Item1, candidates[i].Item2);
+            device.SwapOut(candidates[j].Item1, candidates[j].Item2);
+            device.SwapOut(candidates[k].Item1, candidates[k].Item2);
+            device.SwapOut(candidates[l].Item1, candidates[l].Item2);
         }
     }
 
     private class Gate(string in1, string in2, string @operator, string @out)
     {
+        public Guid Id { get; } = Guid.NewGuid();
         public string In1 { get; } = in1;
         public string In2 { get; } = in2;
         public string Operator { get; } = @operator;
         public string Out { get; set; } = @out;
+        public override int GetHashCode() => this.Id.GetHashCode();
+    }
+
+    private class Device
+    {
+        private readonly Dictionary<string, Gate> gateByOut;
+        private readonly Gate[] gates;
+        private readonly Dictionary<string, HashSet<Gate>> gatesByIn;
+
+        public Device(Gate[] gates)
+        {
+            this.gates = gates;
+            this.gateByOut = gates.ToDictionary(x => x.Out);
+            this.gatesByIn = gates
+                .Select(x => (In: x.In1, Gate: x))
+                .Concat(gates.Select(x => (In: x.In2, Gate: x)))
+                .GroupBy(x => x.In)
+                .ToDictionary(x => x.Key, x => x.Select(y => y.Gate).ToHashSet());
+        }
+
+        public IReadOnlyCollection<Gate> Gates => this.gates;
+
+        public void SwapOut(Gate gate1, Gate gate2)
+        {
+            (gate1.Out, gate2.Out) = (gate2.Out, gate1.Out);
+            this.gateByOut[gate1.Out] = gate1;
+            this.gateByOut[gate2.Out] = gate2;
+        }
+
+        public IEnumerable<string> GetOutsByIn(string @in)
+        {
+            if (!this.gatesByIn.TryGetValue(@in, out var hs))
+            {
+                yield break;
+            }
+
+            foreach (var g in hs)
+            {
+                yield return g.Out;
+            }
+        }
+
+        public Gate GetGateByOut(string @out) => this.gateByOut[@out];
     }
 }
